a04fe8f03a164dbcf408e3ae44475e6a5f898d75
Victor G <victorgj95@outlook.com>
2024-04-03 14:25:01 +0200

    header completed

---
+++ index.js
@@ -0,0 +1,48 @@
+const menuBurgerImg = document.createElement("img");
+menuBurgerImg.className = "menu-burger-img";
+menuBurgerImg.src = "assets/images/menu-burger.png";
+
+const menuBurgerDiv = document.querySelector(".menu-burger");
+menuBurgerDiv.appendChild(menuBurgerImg);
+
+const ytLogoImg = document.createElement("img");
+ytLogoImg.className = "yt-logo";
+ytLogoImg.src = "assets/images/youtube-logo.png";
+
+const ytLogoDiv = document.querySelector(".logo-yt");
+ytLogoDiv.appendChild(ytLogoImg);
+
+const micImg = document.createElement("img");
+micImg.className = "mic-img";
+micImg.src = "assets/images/microphone.png";
+
+const micImgDiv = document.querySelector(".mic-img-div");
+micImgDiv.appendChild(micImg);
+
+const camImg = document.createElement("img");
+camImg.className = "cam-img";
+camImg.src = "assets/images/video-camera.png";
+
+const camImgDiv = document.querySelector(".cam-img-div");
+camImgDiv.appendChild(camImg);
+
+const notifImg = document.createElement("img");
+notifImg.className = "notif-img";
+notifImg.src = "assets/images/notification.png";
+
+const notifImgDiv = document.querySelector(".notif-img-div");
+notifImgDiv.appendChild(notifImg);
+
+const profilePicIcon = document.createElement("img");
+profilePicIcon.className = "profile-picture-icon";
+profilePicIcon.src = "assets/images/user.png";
+
+const profilePicIconDiv = document.querySelector(".profile-picture-icon-div");
+profilePicIconDiv.appendChild(profilePicIcon);
+
+const searchBtnIcon = document.createElement("img");
+searchBtnIcon.className = "search-btn-icon";
+searchBtnIcon.src = "assets/images/search.png";
+
+const searchBtnDiv = document.querySelector(".search-btn");
+searchBtnDiv.appendChild(searchBtnIcon);
