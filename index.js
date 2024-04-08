87b63b38ebddc776e241a8c9967bd537b32ce543
Victor G <victorgj95@outlook.com>
2024-04-08 16:56:08 +0200

    refactored more html elements, fixed style properly to the change

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function createHTMLElement(node, className, parent, src = null) {
   parent.appendChild(element);
 }
 
+// Navigation bar
+
 createHTMLElement(
   "img",
   "menu-burger-img",
@@ -49,6 +51,53 @@ createHTMLElement(
   "search-btn-icon",
   document.querySelector(".search-btn"),
   "assets/images/search.png"
+);
+
+// Lateral menu
+
+createHTMLElement(
+  "div",
+  "home-img-and-text",
+  document.querySelector(".lateral-menu")
+);
+createHTMLElement(
+  "img",
+  "home-svg",
+  document.querySelector(".home-img-and-text"),
+  "assets/images/bouton-daccueil.jpg"
+);
+createHTMLElement(
+  "div",
+  "shorts-img-and-text",
+  document.querySelector(".lateral-menu")
+);
+createHTMLElement(
+  "img",
+  "shorts-svg",
+  document.querySelector(".shorts-img-and-text"),
+  "assets/images/shorts.png"
+);
+createHTMLElement(
+  "div",
+  "subscriptions-img-and-text",
+  document.querySelector(".lateral-menu")
+);
+createHTMLElement(
+  "img",
+  "subscribe-svg",
+  document.querySelector(".subscriptions-img-and-text"),
+  "assets/images/subscription.png"
+);
+createHTMLElement(
+  "div",
+  "you-img-and-text",
+  document.querySelector(".lateral-menu")
+);
+createHTMLElement(
+  "img",
+  "you-svg",
+  document.querySelector(".you-img-and-text"),
+  "assets/images/you.png"
 );
 
 const cardContainer = document.querySelector(".card-container");
@@ -287,23 +336,3 @@ fetch("data.json")
       return data;
     }
   });
-
-// Lateral menu
-
-const selectLateralMenu = document.querySelector(".lateral-menu");
-selectLateralMenu.innerHTML = `
- <div class="home-img-and-text">
- <svg class="home-svg" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: inherit; width: 24px; height: 24px; stroke: white"><path d="m12 4.44 7 6.09V20h-4v-6H9v6H5v-9.47l7-6.09m0-1.32-8 6.96V21h6v-6h4v6h6V10.08l-8-6.96z"></path></svg> <p class="lateral-menu-text">Home</p>
- </div>
- <div class="shorts-img-and-text">
- <svg class="shorts-svg" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: inherit; width: 24px; height: 24px; stroke: white"><path d="M10 14.65v-5.3L15 12l-5 2.65zm7.77-4.33-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zm-.23 5.86-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46c-.99-.41-1.65-1.35-1.7-2.41-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L15.5 9.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56z"></path></svg> <p class="lateral-menu-text">Shorts</p>
- </div>
- <div class="subscriptions-img-and-text">
- <svg class="subscribe-svg" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: inherit; width: 24px; height: 24px; stroke: white;"><path d="M20 7H4V6h16v1zm2 2v12H2V9h20zm-7 6-5-3v6l5-3zm2-12H7v1h10V3z"></path></svg>
- <p class="lateral-menu-text">Subscriptions</p>
- </div>
- <div class="you-img-and-text">
- <svg class="you-svg" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: inherit; width: 24px; height: 24px; stroke: white;"><path d="M4 20h14v1H3V6h1v14zM21 3v15H6V3h15zm-4 7.5L11 7v7l6-3.5z"></path></svg>
- <p class="lateral-menu-text">You</p>
- </div>
- `;
